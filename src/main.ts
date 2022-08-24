6e595189d4ba7d766738ed975a17fb7579e93f0e
Daiki48 <daiki48.engineer@gmail.com>
2022-08-24 14:48:46 +0900

    [update] tree view completion

--- src/main.ts
+++ src/main.ts
@@ -7,13 +7,19 @@ const tree = async (root: string, prefix = "") => {
 		const treeEntry = { ...entry, path: join(root, entry.name) };
 		entries.push(treeEntry);
 	}
+	if (entries.length == 0) {
+		return;
+	}
 	const sortedEntries = entries.sort((a, b) =>
 		a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1
 	);
+	const lastOne = sortedEntries[entries.length - 1];
 	for await (const entry of sortedEntries) {
-		console.log(prefix + entry.name);
+		const branch = entry === lastOne ? "┗━━ " : "┣━━ ";
+		console.log(prefix + branch + entry.name);
 		if (entry.isDirectory && entry.name !== ".git") {
-			await tree(entry.path, prefix + "  ");
+			const indent = entry === lastOne ? "    " : "┃   ";
+			await tree(entry.path, prefix + "    ");
 		}
 	}
 };
